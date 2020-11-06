c7a6b3038d1a01b87f7d5fa7d96d2f7bf9f40b5b
jmaxfield2412 <jmaxfield@student.strode-college.ac.uk>
2020-11-06 11:55:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+#Ball pit problem
+#The number of needed to fill a childrens ball pit can be calculated as: the volume of the ball pit divided by the volume of a single ball multiplied by the packing density.
